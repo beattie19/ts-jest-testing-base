f138911a9232bf75fd9301fcfb82a2ca4ddfdfa0
Stephen Beattie <stephen.beattie@estimateone.com>
2023-08-31 13:34:18 +1000

    sprint 2

--- src/tennis-kata.ts
+++ src/tennis-kata.ts
@@ -1,22 +1,54 @@
-export const getResult = (points: [number, number][]) => {
-    if (points.length === 0) {
-        return 'love - love';
+
+export const getResult = (scores: number[]) => {
+    const playerApoints = scores.filter((score) => score   === 0).length;
+    const playerBpoints = scores.filter((score) => score   === 1).length;
+
+    if (playerApoints === playerBpoints) {
+        if (playerApoints === 3 && playerBpoints === 3) {
+            return 'deuce';
+        }
+
+        return `${transformPointsToString(playerApoints)} all`;
     }
-    return `${transformTennisScore(points[0][0])} - ${transformTennisScore(points[0][1])}`;
+
+    if (canWin(playerApoints)) {
+        if (canWin(playerBpoints)) {
+            const advantagePlayer = playerApoints < playerBpoints ? 'Player B' : 'Player A';
+            return `advantage ${advantagePlayer}`;
+        } 
+
+        return 'Game Player A'
+    }
+
+    if (canWin(playerBpoints) && !canWin(playerApoints)) {
+        return 'Game Player B'
+    }
+
+    return generateStringScore(playerApoints, playerBpoints);
 }
 
-const transformTennisScore = (numPoints: number) => {
-   switch (numPoints) {
-    case 0:
-        return 'love';
-        
-    case 1:
-      return '15';
-    case 2:
-        return '30';
-    
-    case 3:
-        return '40';
+const canWin = (points: number) => {
+    return points >= 3;
+}
 
-   }
+const generateStringScore = (playerApoints: number, playerBpoints: number) => {
+
+    const playerApointsString = transformPointsToString(playerApoints);
+    const playerBpointsString = transformPointsToString(playerBpoints);
+    return `${playerApointsString} - ${playerBpointsString}`;
+}
+
+const transformPointsToString = (points: number) => {
+    const pointsMax = points > 3 ? 3 : points;
+    
+    switch (pointsMax) {
+        case 0:
+            return 'love'
+        case 1:
+            return '15'
+        case 2:
+                return '30'
+        case 3:
+                return '40'
+    }
 }
--- tests/tennis-kata.test.ts
+++ tests/tennis-kata.test.ts
@@ -1,27 +1,47 @@
 import { getResult } from '../src/tennis-kata';
 
 describe('Tennis Kata', () => {
-    test('games not started: love - love', () => {
-        expect(getResult([])).toBe('love - love');
+    test('love - love', () => {
+        expect(getResult([])).toBe('love all');
     })
 
-
-    test('love - love', () => {
-        expect(getResult([[0, 0]])).toBe('love - love');
-    });
-    
     test('love - 15', () => {
-        expect(getResult([[0, 1]])).toBe('love - 15');
-    });
+        expect(getResult([1])).toBe('love - 15');
+    })
+
+    test('15 - love', () => {
+        expect(getResult([0])).toBe('15 - love');
+    })
 
     test('love - 30', () => {
-        expect(getResult([[0, 1], [1, 0]])).toBe('love - 30');
-    });
+        expect(getResult([1, 1])).toBe('love - 30');
+    })
 
-    test('love - 40', () => {
-        expect(getResult([[0, 3]])).toBe('love - 40');
-    });
+    test('30 - 30', () => {
+        expect(getResult([1, 0, 1, 0])).toBe('30 all');
+    })
 
+    test('40 all gives deuce', () => {
+        expect(getResult([1, 1, 0, 1, 0, 0])).toBe('deuce');
+    })
 
+    test('advantage player B', () => {
+        expect(getResult([1, 1, 0, 1, 0, 0, 1])).toBe('advantage Player B');
+    })
 
-})
+    test('advantage player A', () => {
+        expect(getResult([0, 0, 1, 0, 1, 1, 0])).toBe('advantage Player A');
+    })
+
+    test('player A wins', () => {
+        expect(getResult([0, 0, 0, 0])).toBe('Game Player A');
+    })
+
+    test('player B wins', () => {
+        expect(getResult([1, 1, 1, 1])).toBe('Game Player B');
+    })
+
+    test('deuce 2', () => {
+        expect(getResult([1, 1, 1, 0, 0, 0, 0, 1])).toBe('deuce');
+    })
+});
